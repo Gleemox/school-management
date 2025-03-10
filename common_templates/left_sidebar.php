23ca1713873bdfe44ab26e4745d2019c06719133
ibuildsoftware <ibuildsoftware@outlook.fr>
2025-03-10 17:18:13 +0100

    Add comments to index and left_sidebar files

--- common_templates/left_sidebar.php
+++ common_templates/left_sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="" class="brand-link">
         <img src="<?= ADMIN_ASSETS_DIR ?>img/AdminLTELogo.png" alt="AdminLTE Logo"
              class="brand-image img-circle elevation-3"
              style="opacity: .8">
@@ -16,7 +16,7 @@
                 <img src="<?= ADMIN_ASSETS_DIR ?>img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block"><?= $_SESSION['name']; ?></a>
+                <a href="#" class="d-block"><?= $_SESSION['name']; // stores user's name ?></a>
             </div>
         </div>
 
--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require 'modules/classes/DB.php';
 require 'common_templates/head.php';
 ?>
 
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="hold-transition sidebar-mini layout-fixed"> <!-- 3 classes mentioned in AdminLTE -->
 <div class="wrapper">
     <!-- Preloader -->
     <!--    <div class="preloader flex-column justify-content-center align-items-center">-->
@@ -81,35 +81,35 @@ require 'common_templates/head.php';
 <!-- overlayScrollbars -->
 <script src="<?php echo ADMIN_URL ?>assets/plugins/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?php echo ADMIN_URL ?>assets/js/adminlte.min.js"></script>
+<script src="<?php echo ADMIN_URL ?>assets/js/adminlte.min.js"></script> <!--main script file -->
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo ADMIN_URL ?>assets/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!--<script src="--><?php //echo ADMIN_URL ?><!--assets/js/dashboard.js"></script>-->
 
-<script src="<?php echo ADMIN_URL ?>assets/js/jquery.validate.min.js"></script>
-<script src="<?php echo ADMIN_URL ?>assets/js/additional-methods.min.js"></script>
-<script src="<?php echo ADMIN_URL ?>assets/js/toastr.min.js"></script>
+<script src="<?php echo ADMIN_URL ?>assets/js/jquery.validate.min.js"></script> <!-- for validation -->
+<script src="<?php echo ADMIN_URL ?>assets/js/additional-methods.min.js"></script> <!-- for putting numbers -->
+<script src="<?php echo ADMIN_URL ?>assets/js/toastr.min.js"></script> <!-- for showing messages -->
 
 
-<script src="<?php echo ADMIN_URL ?>assets/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo ADMIN_URL ?>assets/js/jquery.dataTables.min.js"></script> <!-- for creating tables using dataTables -->
 <script src="<?php echo ADMIN_URL ?>assets/js/dataTables.bootstrap4.min.js"></script>
 <script src="<?php echo ADMIN_URL ?>assets/js/dataTables.responsive.min.js"></script>
 <script src="<?php echo ADMIN_URL ?>assets/js/responsive.bootstrap4.min.js"></script>
 <script src="<?php echo ADMIN_URL ?>assets/js/dataTables.buttons.min.js"></script>
 <script src="<?php echo ADMIN_URL ?>assets/js/buttons.bootstrap4.min.js"></script>
-<script src="<?php echo ADMIN_URL ?>assets/js/custom_script.js"></script>
-<script src="<?php echo ADMIN_URL ?>assets/js/index.js"></script>
+<script src="<?php echo ADMIN_URL ?>assets/js/custom_script.js"></script> <!-- I created this file -->
+<script src="<?php echo ADMIN_URL ?>assets/js/index.js"></script> <!-- I created this file -->
 
 <script>
     $(function () {
-        $('.select2').select2();
+        $('.select2').select2(); // integrate select2 jquery
 
-        $('.select2bs4').select2({
+        $('.select2bs4').select2({ // integrate select2bs4
             theme: 'bootstrap4'
         });
 
-        $('[data-mask]').inputmask();
+        $('[data-mask]').inputmask(); // inputmasking using data mask
     });
 </script>
 </body>
